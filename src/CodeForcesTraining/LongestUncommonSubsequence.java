65a91aa683ae84d6ecf86b368526f69aca3efa31
Naveen <nk0187912@gmail.com>
2021-12-14 00:47:07 +0530

    got an idea, I'll keep this code for later while working on new idea

---
+++ src/CodeForcesTraining/LongestUncommonSubsequence.java
@@ -0,0 +1,35 @@
+package CodeForcesTraining;
+//https://codeforces.com/contest/766/problem/A
+import java.util.*;
+
+public class LongestUncommonSubsequence {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String str1 = input.next();
+    String str2 = input.next();
+
+    if(str1.equals(str2)) {
+      System.out.println(-1);
+      return;
+    }
+
+   // int i = 0, j = 0, len = 0;
+//
+//    while(i<str1.length() && j<str2.length()){
+//
+//      if(str1.charAt(i) != str2.charAt(j));
+//      len++;
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+  }
+}
