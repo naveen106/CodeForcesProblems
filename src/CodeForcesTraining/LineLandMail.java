d3a7665c79c268427db0fb7850d1f5260087e093
Naveen <nk0187912@gmail.com>
2021-12-14 10:12:20 +0530

    solution to problem 567A

--- src/CodeForcesTraining/LineLandMail.java
+++ src/CodeForcesTraining/LineLandMail.java
@@ -13,22 +13,21 @@ public class LineLandMail {
 
     int finalMin, finalMax;
     for(int i = 0; i<n; i++){
-        int min1 = Integer.MAX_VALUE;
+      int min1 = Integer.MAX_VALUE;
 
-        if(i != n-1)
-        min1 = Math.abs(arr[i] - arr[i+1]);
+      if(i != n-1)
+      min1 = Math.abs(arr[i] - arr[i+1]);
 
-        if(i>0) {
-          int min2 = Math.abs(arr[i] - arr[i - 1]);
-          finalMin = Math.min(min1, min2);
-        }
-        else  //runs only once
-        finalMin = min1;
+      if(i>0) {
+        int min2 = Math.abs(arr[i] - arr[i - 1]);
+        finalMin = Math.min(min1, min2);
+      }
+      else  //runs only once
+      finalMin = min1;
 
-        int max1 = Math.abs(arr[i] - arr[0]);
-        int max2 = Math.abs(arr[i] - arr[n-1]);
-        finalMax = Math.max(max1, max2);
-
+      int max1 = Math.abs(arr[i] - arr[0]);
+      int max2 = Math.abs(arr[i] - arr[n-1]);
+      finalMax = Math.max(max1, max2);
       System.out.println(finalMin+" "+finalMax);
     }
   }
