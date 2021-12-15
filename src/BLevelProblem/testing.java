f1779fd0b54f5ff2a939317bb4ccf0c0084d2fdd
Naveen <nk0187912@gmail.com>
2021-12-15 18:50:59 +0530

    a file made to test any code or idea I come up with to solve a problem

--- src/BLevelProblem/testing.java
+++ src/BLevelProblem/testing.java
@@ -3,28 +3,28 @@ import java.util.*;
 import java.lang.Math;
 public class testing {
   public static void main(String[] args) {
-    Scanner input = new Scanner (System.in);
-    int n = input.nextInt();
-    int m = input.nextInt();
 
-    if(n == m){
-      System.out.println("YES");
-      return;
+    int[]slimes = {13, 57, 43, 50, 10};
+    int n = 5;
+    Arrays.sort(slimes);
+
+    int temp = slimes[slimes.length-1] + slimes[slimes.length-2];
+    int cost = slimes[slimes.length-1] * slimes[slimes.length-2];
+
+    for(int i = slimes.length-3; i>=0; i--){
+
+      if(temp < 100){
+        temp  = slimes[i] + temp;
+        cost += slimes[i] * temp;
+        i--;
+      }
+
+      else{
+        temp = slimes[i] + slimes[i-1];
+        cost+= slimes[i] * slimes[i-1];
+        i--;
+      }
     }
-
-    StringBuilder first = new StringBuilder(Integer.toBinaryString(n));
-    first = first.reverse();
-
-    int num = Integer.parseInt(first.toString());
-    int newNum = 0;
-
-    int i = 0;
-    while(num!=0){
-      newNum += num%10 * Math.pow(2,i);
-      num=num/10;
-      ++i;
-    }
-    System.out.println(newNum);
-
+    System.out.println(cost);
   }
 }
