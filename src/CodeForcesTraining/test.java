cf15e8d66130d5f38fc5bcf64f6f1e0fdcf0cc26
Naveen <nk0187912@gmail.com>
2021-12-20 02:10:20 +0530

    debug/tester file

--- src/CodeForcesTraining/test.java
+++ src/CodeForcesTraining/test.java
@@ -2,31 +2,27 @@ package CodeForcesTraining;
 import java.util.*;
 public class test {
     public static void main(String[] args) {
-      Scanner input = new Scanner(System.in);
-      int n = input.nextInt();
-      int k = input.nextInt();
+      int[]price = {60, 70, 80, 100, 90, 75, 80, 120};
+      int[] arr = new int[price.length];
+      Stack<Integer> stack = new Stack<>();
 
-      String[] arr = new String[n+k];
+      stack.push(0);
+      int top = 0;
+      arr[0] = 1;
 
-      for(int i = 0; i<n; i++)
-        arr[i] = input.next();
+      for(int i = 1; i<price.length; i++){
+        top = stack.pop();
+        if(price[i] > price[top]){
+          arr[i] = i+1;
+          stack.push(i);
+        }
 
-      int count=0;
-
-      for(int i = 0; i<arr.length-k; i++){
-
-        int j = i+1;
-        int count2 = 0;
-
-        while(count2 < k){
-
-          if(arr[j]!= null && arr[i].length() == arr[j].length())
-            count++;
-          j++;
-          count2++;
+        else{
+          arr[i] = 1;
+//                stack.push(top);
+          stack.push(i);
         }
       }
-
-      System.out.println(count);
-  }
+      System.out.println(Arrays.toString(arr));
+    }
 }
