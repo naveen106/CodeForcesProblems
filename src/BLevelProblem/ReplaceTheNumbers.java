ab69e705e9cee119c0a1c44c86c158be5ad52c79
Naveen <nk0187912@gmail.com>
2021-12-20 02:12:00 +0530

    will do later

--- src/BLevelProblem/ReplaceTheNumbers.java
+++ src/BLevelProblem/ReplaceTheNumbers.java
@@ -4,27 +4,30 @@ public class ReplaceTheNumbers {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     int q = input.nextInt();
-    int arr[] = new int[q];
-    int count = 0;
+    int []arr = new int[q];
+    String []arr2 = new String[q];
+    ArrayList<Integer>count = new ArrayList<>();
+
+
     for(int i = 0; i<q; i++){
-      if(count == q)
-        break;
 
       int first = input.nextInt();
-      if(first == 1){
-        arr[i] = input.nextInt();
-      }
+      if(first == 1)
+      arr[i] = input.nextInt();
+
       else{
         int second = input.nextInt();
         int third = input.nextInt();
-        for(int j = 0; j<i && arr[j]!=0; j++){
-          if(arr[j] == second)
-            arr[j] = third;
-        }
-        i--;
+        arr2[second] = arr2[second].concat(Integer.toString(third));
+        count.add(i);
       }
-      count++;
     }
+
+    for(int i = 0; i<arr.length && arr[i]!=0; i++){
+
+    }
+
+
 
     for(int i = 0; i<arr.length && arr[i]!=0; i++){
       System.out.print(arr[i] + " ");
