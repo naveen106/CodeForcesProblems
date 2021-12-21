d017c9945d4afd4e0a57df9e1029f8b70a5c743a
Naveen <nk0187912@gmail.com>
2021-12-22 00:49:11 +0530

    tester file

--- src/BLevelProblem/testing.java
+++ src/BLevelProblem/testing.java
@@ -2,29 +2,18 @@ package BLevelProblem;
 import java.util.*;
 import java.lang.Math;
 public class testing {
-  public static void main(String[] args) {
 
-    int[]slimes = {13, 57, 43, 50, 10};
-    int n = 5;
-    Arrays.sort(slimes);
-
-    int temp = slimes[slimes.length-1] + slimes[slimes.length-2];
-    int cost = slimes[slimes.length-1] * slimes[slimes.length-2];
-
-    for(int i = slimes.length-3; i>=0; i--){
-
-      if(temp < 100){
-        temp  = slimes[i] + temp;
-        cost += slimes[i] * temp;
-        i--;
-      }
-
-      else{
-        temp = slimes[i] + slimes[i-1];
-        cost+= slimes[i] * slimes[i-1];
-        i--;
-      }
+  static int fibonacci(int n){
+    if(n<=1){
+      return n;
     }
-    System.out.println(cost);
+    return fibonacci(n - 2) + fibonacci(n - 1);
+  }
+  public static void main(String[] args) {
+    int n = 10;
+    int t = n;
+    for(int i = 1; i<=n; i++)
+    System.out.print(fibonacci(i) + " ");
+
   }
 }
