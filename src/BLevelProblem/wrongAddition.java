506454b6349e0b3691e2b085660b3f1f9b49ccd1
Naveen <nk0187912@gmail.com>
2021-12-21 19:16:20 +0530

    1619D2C will come back later

---
+++ src/BLevelProblem/wrongAddition.java
@@ -0,0 +1,12 @@
+package BLevelProblem;
+import java.util.*;
+public class wrongAddition {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int t = input.nextInt();
+
+    while(t-- > 0){
+
+    }
+  }
+}
