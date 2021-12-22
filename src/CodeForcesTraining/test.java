b9f4647be3d058f6dfd443f5e8e8e85ccc1c88ac
Naveen <nk0187912@gmail.com>
2021-12-23 03:56:24 +0530

    tester/debug file

--- src/CodeForcesTraining/test.java
+++ src/CodeForcesTraining/test.java
@@ -2,6 +2,7 @@ package CodeForcesTraining;
 import java.util.*;
 public class test {
     public static void main(String[] args) {
-
+        char n = '5';
+        System.out.println((int)n);
     }
 }
