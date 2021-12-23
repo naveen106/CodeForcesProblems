d21564d6ee01df5d4404df0a29f19c0d509d6cd0
Naveen <nk0187912@gmail.com>
2021-12-24 00:39:51 +0530

    1446D1A

--- src/CodeForcesTraining/KnapSack.java
+++ src/CodeForcesTraining/KnapSack.java
@@ -1,8 +1,82 @@
 package CodeForcesTraining;
 import java.util.*;
+import java.io.*;
+//THE SOLUTION WAS NOT DONE BY ME, BUT I LEARNED A LOOOOOOT FROM THIS PROBLEM
 //https://codeforces.com/contest/1446/problem/A
 public class KnapSack {
   public static void main(String[] args) {
+    FastScanner fs = new FastScanner();
+    int T = fs.nextInt();
+    PrintWriter out = new PrintWriter(System.out);
+    for (int tt = 0; tt < T; tt++) {
+      int n = fs.nextInt();
+      long W = fs.nextLong();
+      Item toTake = null;
+      ArrayList<Item> small = new ArrayList<>();
+      long smallTotal = 0;
+      for (int i = 0; i < n; i++) {
+        int weight = fs.nextInt();
+        if (weight > W) continue;
+        if (weight >= (W + 1) / 2) {
+          toTake = new Item(i + 1, weight);
+        } else {
+          if (smallTotal + weight <= W) {
+            small.add(new Item(i + 1, weight));
+            smallTotal += weight;
+          }
+        }
+      }
+      if (toTake != null) {
+        out.println(1);
+        out.println(toTake.id);
+      } else if (smallTotal >= (W + 1) / 2) {
+        out.println(small.size());
+        for (Item i : small) {
+          out.print(i.id + " ");
+        }
+        out.println();
+      } else
+        out.println(-1);
+    }
+    out.close();
+  }
+
+  static class Item {
+    int id, weight;
+    public Item(int id, int weight) {
+      this.id=id;
+      this.weight=weight;
+    }
+  }
+
+  static class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+      while (!st.hasMoreTokens())
+        try {
+          st=new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+    int[] readArray(int n) {
+      int[] a=new int[n];
+      for (int i=0; i<n; i++) a[i]=nextInt();
+      return a;
+    }
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  }
+  /*
+  public static void main(String[] args) {
+
     Scanner input = new Scanner(System.in);
     int t = input.nextInt();
 
@@ -10,18 +84,18 @@ public class KnapSack {
 
       int n = input.nextInt();
       long w = input.nextLong();
-      int[]arr = new int[n];
-      int min = Integer.MAX_VALUE;
-      long c = w/2;
+      long[]arr = new long[(int)n];
+      long min = Integer.MAX_VALUE;
+      long c = (w+1)/2;
 
       for(int i=0; i<n;i++){
-        arr[i] = input.nextInt();
+        arr[i] = input.nextLong();
         min = Math.min(min, arr[i]);
       }
 
-      if(min <= w && min > c){
+      int temp=0;
+      if(min <= w && min >= c){
         System.out.println(1);
-        int temp=0;
         for(int i = 0; i<arr.length; i++){
           if(arr[i] == min){
             temp = i;
@@ -29,19 +103,24 @@ public class KnapSack {
           }
         }
         System.out.println(++temp);
-        continue;
       }
 
       ArrayList<Integer> list = new ArrayList<>();
-
+      long sum = 0;
       for(int i = 0; i<n && c>0; i++){
+
         if(arr[i] <= c){
           c-=arr[i];
+          sum+=arr[i];
           list.add(i+1);
         }
       }
 
-      if(list.size() == 0) {
+      if(temp != 0){
+        continue;
+      }
+
+      if(list.size() == 0 || sum < c || sum > w) {
         System.out.println(-1);
         continue;
       }
@@ -50,59 +129,8 @@ public class KnapSack {
       for(int element : list)
         System.out.print(element + " ");
       System.out.println();
-//      int min = Integer.MAX_VALUE;
-//
-//      for(int i = 0; i<n; i++){
-//        arr[i] = input.nextInt();
-//        min = Math.min(min, arr[i]);
-//      }
-//      int c = 0;
-//      if((w&1) == 0)
-//      c = w/2+1;
-//
-//      else
-//      c = w/2;
-//
-//      if(min > w){
-//        System.out.println(-1);
-//        continue;
-//      }
-//
-//      if(min == c || min == w){
-//        int temp=0;
-//        System.out.println(1);
-//        for(int i = 0; i<n; i++) {
-//          if (arr[i] == min) {
-//            temp = i;
-//            break;
-//          }
-//        }
-//        System.out.println(++temp);
-//        continue;
-//      }
-//
-//      ArrayList<Integer> list = new ArrayList<>();
-//
-//      for(int i = 0; i<arr.length && c!=0 || w!=0; i++){
-//        if(arr[i] <= c ) {
-//          c -= arr[i];
-//          w -= arr[i];
-//          list.add(i+1);
-//        }
-//      }
-//      System.out.println(list.size());
-//      for(int element: list)
-//        System.out.print(element + " ");
+
     }
   }
+  */
 }
-
-//1
-//3
-//-1
-//1
-//1
-//-1
-//1
-//1
-//-1
