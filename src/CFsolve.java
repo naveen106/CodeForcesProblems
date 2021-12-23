209f97466d44753b832c1d1ee83ccd52dd1aa135
Naveen <nk0187912@gmail.com>
2021-12-24 01:17:52 +0530

    will write code in this template from now on

--- src/CFsolve.java
+++ src/CFsolve.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class CFsolve {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CFsolve {
+  public static void main(String[] args) {
+    PrintWriter out = new PrintWriter(System.out);
+    FastScanner input = new FastScanner();
+    //WRITE YOUR CODE HERE // use - out.println() or out.print() to print. //
+
+
+
+
+  }
+
+  static class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+      while (!st.hasMoreTokens())
+        try {
+          st=new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+    int[] readArray(int n) {
+      int[] a=new int[n];
+      for (int i=0; i<n; i++) a[i]=nextInt();
+      return a;
+    }
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  }
 }
