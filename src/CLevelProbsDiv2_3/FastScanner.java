4b8907c6ca3d15c4a145d858f93b820e4d39b7a8
Naveen <nk0187912@gmail.com>
2021-12-24 19:00:56 +0530

    FastScanner class to take input faster

--- src/CLevelProbsDiv2_3/FastScanner.java
+++ src/CLevelProbsDiv2_3/FastScanner.java
@@ -1,9 +1,6 @@
 package CLevelProbsDiv2_3;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class FastScanner {
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
@@ -26,6 +23,7 @@ public class FastScanner {
       for (int i=0; i<n; i++) a[i]=nextInt();
       return a;
     }
+    byte nextByte(){return Byte.parseByte(next());}
     long nextLong() {
       return Long.parseLong(next());
     }
