161b693fd53a472adee19bdc5fe9611e9d5ec660
Naveen <nk0187912@gmail.com>
2021-12-26 20:58:31 +0530

    halfway

--- src/CLevelProbsDiv2_3/PoisonedDagger.java
+++ src/CLevelProbsDiv2_3/PoisonedDagger.java
@@ -1,56 +1,94 @@
 package CLevelProbsDiv2_3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class PoisonedDagger {
   public static void main(String[] args) {
     FastScanner input = new FastScanner();
     PrintWriter out = new PrintWriter(System.out);
+    int t = input.nextInt();
+    while(t-- > 0){
+      byte n = input.nextByte();
+      long health = input.nextLong();
 
-    byte n = input.nextByte();
-    long health = input.nextLong();
+      int min;
+      if (health % n == 0)
+        min = (int) health / n;
+      else
+        min = (int) health / n + 1;
 
-    int k = 0;
-    if(health%n == 0)
-      k = (int)health/n;
-    else
-      k = (int)health/n+1;
+      long damage = 0;
 
-    long damageDealt = 0;
-    int[]seconds = new int[n];
+      int[] time = new int[n];
 
-    for(int i = 0; i<n; i++)
-      seconds[i] = input.nextInt();
+      for (int i = 0; i < n; i++)
+        time[i] = input.nextInt();
 
-    int i = 0;
-    int tempIdx = 0;
-    long tempDamage = 0;
-    while(damageDealt < health){
-      if(i+1 < n && seconds[i+1] - seconds[i] >= k) {
-        if(tempDamage == 0)
-          tempDamage = damageDealt;
-        damageDealt += k;
-        if(tempIdx == 0)
-          tempIdx = i;
+      int constIdx = 0;
+      long constDamage = 0;
+      for (int i = 1; i < n; i++) {
+        int temp = time[i] - time[i - 1];
+
+        if (temp > min) {
+          if (constIdx == 0)
+            constIdx = i-1;
+          if (constDamage == 0)
+            constDamage = damage;
+          damage += min;
+        } else
+          damage += temp;
       }
-      else if(i+1 < n)
-        damageDealt += seconds[i+1]-seconds[i];
-      else if(i==n-1)
-        damageDealt +=k;
-
-      if(i==n-1 && damageDealt < health){
-        i = tempIdx-1;
-        damageDealt = tempDamage;
-        k++;
+      damage += min;
+      min++;
+      while (damage < health) {
+        damage = constDamage;
+        for (int i = Math.max(constIdx,0); i < n; i++) {
+          if (i+1 < n && time[i+1]-time[i] > min)
+            damage += min;
+          else if(i+1 < n)
+            damage += time[i+1]-time[i];
+        }
+        damage += min;
+        min++;
       }
-      i++;
+      if(health <= n)
+        System.out.println(1);
+      else
+      System.out.println(--min);
     }
-    out.println(k);
-
   }
 
+  static class FastScanner {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer("");
+
+    String next() {
+      while (!st.hasMoreTokens())
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    int[] readArray(int n) {
+      int[] a = new int[n];
+      for (int i = 0; i < n; i++) a[i] = nextInt();
+      return a;
+    }
+
+    byte nextByte() {
+      return Byte.parseByte(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+  }
 }
