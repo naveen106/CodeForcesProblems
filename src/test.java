23f411dfad29b5588174c09bd86d70b133f2cfe6
Naveen <nk0187912@gmail.com>
2022-01-09 11:35:00 +0530

    another tester file

---
+++ src/test.java
@@ -0,0 +1,122 @@
+/*
+ID: nk018791
+LANG: JAVA
+TASK: test
+ */
+
+import java.util.*;
+import java.io.*;
+public class test {
+    public static void main(String[] args) {
+        FastScanner input = new FastScanner();
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int i = input.nextInt();
+        int j = input.nextInt();
+
+        out.println(i+j);
+        out.close();
+    }
+
+    static int LCM(int a, int b){
+        return a/gcd(a,b) * b;
+    }
+    static long LCM(long a, long b){
+        return (a/gcd(a,b) * b);
+    }
+
+    static long pow(long a, long b) {
+        long res = 1;
+        while (b > 0) {
+            if ((b & 1)!=0)
+                res = res * a;
+            a = a * a;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    static int pow(int a, int b) {
+        int res = 1;
+        while (b > 0) {
+            if ((b & 1)!=0)
+                res = res * a;
+            a = a * a;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    static int gcd(int a,int b){
+        while(b>0){
+            a%=b;
+            a=a^b;
+            b=a^b;
+            a=a^b;
+        }
+        return a;
+    }
+
+    static long gcd(long a,long b){
+        while(b>0){
+            a%=b;
+            a=a^b;
+            b=a^b;
+            a=a^b;
+        }
+        return a;
+    }
+
+    static void swap(int a, int b){
+        a = a^b;
+        b = a^b;
+        a = a^b;
+    }
+
+    static void sort(int[] a) {
+        ArrayList<Integer> l=new ArrayList<>();
+        for (int i:a) l.add(i);
+        Collections.sort(l);
+        for (int i=0; i<a.length; i++) a[i]=l.get(i);
+    }
+
+    static class FastScanner {
+        BufferedReader br;//InputStreamReader(System.in));
+
+        {
+            try {
+                br = new BufferedReader(new FileReader("test.in"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        StringTokenizer st=new StringTokenizer("");
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        int[] readArray(int n) {
+            int[] a=new int[n];
+            for (int i=0; i<n; i++) a[i]=nextInt();
+            return a;
+        }
+        byte nextByte(){return Byte.parseByte(next());}
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
