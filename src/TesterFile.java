e7d6bdde587d1a9b6ea3ba0f7009ed3f3451a23f
Naveen <nk0187912@gmail.com>
2022-02-22 15:09:55 +0530

    Tester file

---
+++ src/TesterFile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Show{
+        static void display(){
+           System.out.println("My name is Naveen Kumar");
+       }
+}
+public class TesterFile{
+    public static void main(String[] args){
+        Show obj = new Show();
+        obj.display();
+    }
+}
