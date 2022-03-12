529cc2c680af148e6f23f81cf1ae00f4baa91e8e
Naveen <nk0187912@gmail.com>
2022-03-12 23:44:34 +0530

    nothing much, solved some doubts and questions

--- src/TesterFile.java
+++ src/TesterFile.java
@@ -1,13 +1,26 @@
 import java.util.*;
 
-class Show{
-        static void display(){
-           System.out.println("My name is Naveen Kumar");
-       }
+class Calculation {
+    int z;
+    public void addition(int x, int y) {
+        z = x + y;
+        System.out.println("The sum of the given numbers:"+z);
+    }
+    public void Subtraction(int x, int y) {
+        z = x - y;
+        System.out.println("The difference between the given numbers:"+z);
+    }
 }
-public class TesterFile{
-    public static void main(String[] args){
-        Show obj = new Show();
-        obj.display();
+public class My_Calculation extends Calculation {
+    public void multiplication(int x, int y) {
+        z = x * y;
+        System.out.println("The product of the given numbers:"+z);
+    }
+    public static void main(String args[]) {
+        int a = 20, b = 10;
+        My_Calculation demo = new My_Calculation();
+        demo.addition(a, b);
+        demo.Subtraction(a, b);
+        demo.multiplication(a, b);
     }
 }
