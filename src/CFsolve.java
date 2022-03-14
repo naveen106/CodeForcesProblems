9b5fda1ceffdc955f45d2eb2406d57deeedf8123
Naveen <nk0187912@gmail.com>
2022-03-14 23:31:00 +0530

    template for solving problems

--- src/CFsolve.java
+++ src/CFsolve.java
@@ -1,132 +1,106 @@
-  import java.util.*;
-  import java.io.*;
+import java.util.*;
+import java.io.*;
 
-  public class CFsolve {
-    static PrintWriter out = new PrintWriter(System.out);
+public class CFsolve {
+  static PrintWriter out = new PrintWriter(System.out);
   
-    static int[][] subsets(int input[]) {
-      return helper(0,input);
+  public static void main(String[] args) {
+    FastScanner input = new FastScanner();
+    StringBuilder str = new StringBuilder(input.next());
+    StringBuilder temp = new StringBuilder(str);
+    
+    out.println(str.append(temp.reverse()));
+    
+    out.close();
+  }
+  
+  
+  static int LCM(int a, int b){
+    return a/gcd(a,b) * b;
+  }
+  static long LCM(long a, long b){
+    return (a/gcd(a,b) * b);
+  }
+  
+  static long pow(long a, long b) {
+    long res = 1;
+    while (b > 0) {
+      if ((b & 1)!=0)
+        res = res * a;
+      a = a * a;
+      b >>= 1;
     }
+    return res;
+  }
   
-    private static int[][] helper(int startIndex, int[]input) {
+  static int pow(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+      if ((b & 1)!=0)
+        res = res * a;
+      a = a * a;
+      b >>= 1;
+    }
+    return res;
+  }
   
-      if (startIndex == input.length) {
-        //int arr = new int[0][0];
-        return new int[0][0];
-      }
+  static int gcd(int a,int b){
+    while(b>0){
+      a%=b;
+      a=a^b;
+      b=a^b;
+      a=a^b;
+    }
+    return a;
+  }
   
-      int[][] smallOutput = helper(startIndex + 1, input);
-      int[][] output = new int[smallOutput.length * 2][];
-      int start = 0;
+  static long gcd(long a,long b){
+    while(b>0){
+      a%=b;
+      a=a^b;
+      b=a^b;
+      a=a^b;
+    }
+    return a;
+  }
   
-      for (int i = 0; i < smallOutput.length; i++) {
-        output[start++] = smallOutput[i];
-      }
+  static void swap(int a, int b){
+    a = a^b;
+    b = a^b;
+    a = a^b;
+  }
   
-      for (int i = 0; i < smallOutput.length; i++) {
-        output[start] = new int[smallOutput[i].length+1];
-        output[start][0] = input[startIndex];
-        
-        for (int j = 0; j<smallOutput[i].length; j++) {
-          output[start][j+1] = smallOutput[i][j];
+  static void sort(int[] a) {
+    ArrayList<Integer> l=new ArrayList<>();
+    for (int i:a) l.add(i);
+    Collections.sort(l);
+    for (int i=0; i<a.length; i++) a[i]=l.get(i);
+  }
+  
+  static class FastScanner {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st=new StringTokenizer("");
+    String next() {
+      while (!st.hasMoreTokens())
+        try {
+          st=new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-        start++;
-      }
-  
-      return output;
+      return st.nextToken();
     }
-      public static void main(String[] args) {
-      FastScanner input = new FastScanner();
-  
-      out.close();
+    
+    int nextInt() {
+      return Integer.parseInt(next());
     }
-
-    static int LCM(int a, int b){
-      return a/gcd(a,b) * b;
-    }
-    static long LCM(long a, long b){
-      return (a/gcd(a,b) * b);
-    }
-
-    static long pow(long a, long b) {
-      long res = 1;
-      while (b > 0) {
-        if ((b & 1)!=0)
-          res = res * a;
-        a = a * a;
-        b >>= 1;
-      }
-      return res;
-    }
-
-    static int pow(int a, int b) {
-      int res = 1;
-      while (b > 0) {
-        if ((b & 1)!=0)
-          res = res * a;
-        a = a * a;
-        b >>= 1;
-      }
-      return res;
-    }
-
-    static int gcd(int a,int b){
-      while(b>0){
-        a%=b;
-        a=a^b;
-        b=a^b;
-        a=a^b;
-      }
+    int[] readArray(int n) {
+      int[] a=new int[n];
+      for (int i=0; i<n; i++) a[i]=nextInt();
       return a;
     }
-
-    static long gcd(long a,long b){
-      while(b>0){
-        a%=b;
-        a=a^b;
-        b=a^b;
-        a=a^b;
-      }
-      return a;
-    }
-
-    static void swap(int a, int b){
-      a = a^b;
-      b = a^b;
-      a = a^b;
-    }
-
-    static void sort(int[] a) {
-      ArrayList<Integer> l=new ArrayList<>();
-      for (int i:a) l.add(i);
-      Collections.sort(l);
-      for (int i=0; i<a.length; i++) a[i]=l.get(i);
-    }
-
-    static class FastScanner {
-      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-      StringTokenizer st=new StringTokenizer("");
-      String next() {
-        while (!st.hasMoreTokens())
-          try {
-            st=new StringTokenizer(br.readLine());
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        return st.nextToken();
-      }
-
-      int nextInt() {
-        return Integer.parseInt(next());
-      }
-      int[] readArray(int n) {
-        int[] a=new int[n];
-        for (int i=0; i<n; i++) a[i]=nextInt();
-        return a;
-      }
-      byte nextByte(){return Byte.parseByte(next());}
-      long nextLong() {
-        return Long.parseLong(next());
-      }
+    byte nextByte(){return Byte.parseByte(next());}
+    long nextLong() {
+      return Long.parseLong(next());
     }
   }
+}
