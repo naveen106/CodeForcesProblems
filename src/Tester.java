d4e5daead7da84ec7ea6b8e9527c70870e6d4161
Naveen <nk0187912@gmail.com>
2022-03-15 21:30:21 +0530

    Testing solution to a problem

--- src/Tester.java
+++ src/Tester.java
@@ -3,54 +3,113 @@ import java.io.*;
 
 public class Tester {
     
-    
-    public boolean isPalindromePair(ArrayList<String> words) {
-        HashMap<String,Integer> map = new HashMap<>();
-        HashSet<String> set = new HashSet<>();
+    static PrintWriter out = new PrintWriter(System.out);
+    public static void placeNQueens(int n){
+        if(n<4)
+            return;
         
-        for(int i = 0; i<words.size(); i++){
-            String temp = words.get(i);
-            
-            StringBuilder str = new StringBuilder(temp);
-            str.reverse();
-            
-            if(str.toString()==temp)
-                return true;
-            else{
-                map.put(str.toString(),1);
-                set.add(str.toString());
-            }
-            
-        }
+        int[][] matrix = new int[n][n];
         
-        for(String str : map.keySet()){
-            if(set.contains(str))
-                return true;
-            
-            else{
-                int length = str.length();
-                
-                for(int i = 0; i<length-1; i++){
-                    str=str.substring(1);
-                    if(set.contains(str))
-                        return true;
-                }
-            }
-            
+        
+        //going to test for position of queen in first row, which means every possible desired outcome
+        for(int j = 0; j<n; j++){
+            int i = 0;
+            fill(matrix,i,j,n);
+            printValues(matrix);
+            matrix = new int[n][n];
         }
-        return false;
+        out.close();
+        
     }
     
-    public static void main(String[] args) {
-        String[]input = {"do","dont","no","not","note","notes","den"};
-        String word = "de";
+    static void printValues(int[][] matrix){
+        
+        for(int i=0; i<matrix.length; i++){
+            for(int j = 0; j<matrix.length; j++){
+                out.print(matrix[i][j] + " ");
+            }
+        }
+        out.println();
+    }
     
-        for(int i = 0; i<input.length; i++){
-            String str = input[i];
-            for(int j=0;j<str.length();j++)
-            if(str.startsWith(word,j))
-                System.out.println(str);
+    static  void fill(int[][]matrix, int r, int c, int n){
+        
+        HashSet<Integer> set = new HashSet<>();
+        set.add(c);
+        
+        matrix[r][c] = 1;
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(isSafe(matrix,i,j,n,set)){
+                    set.add(j);
+                    matrix[i][j] = 1;
+                }
+            }
         }
         
+    }
+    
+    //(m,n) are the coordinate of the current position which is to be checked
+    static boolean isSafe(int[][]matrix, int r, int c, int n, HashSet<Integer> set){
+        
+        int i = r, j = c;
+        
+        //for top and bottom (set has values of j which means index of the coloumn in the matrix which already has a queen)
+        if(set.contains(j))
+            return false;
+        
+        //for top-left direction
+        
+        
+        for(; i>=0; i--){
+            
+            for(;j>=0;j--){
+                if(matrix[i][j] == '1')
+                    return false;
+            }
+        }
+        
+        i=r;j=c;
+        
+        //for top-right direction
+        for(; i>=0; i--){
+            for(;j<n;j++){
+                if(matrix[i][j] == '1')
+                    return false;
+            }
+        }
+        
+        i=r;j=c;
+        //for bottom-left direction
+        for(; i<n; i++){
+            for(;j>=0;j--){
+                if(matrix[i][j] == '1')
+                    return false;
+            }
+        }
+        
+        i=r;j=c;
+        //for bottom-right direction
+        for(; i<n; i++){
+            for(;j<n;j++){
+                if(matrix[i][j] == '1')
+                    return false;
+            }
+        }
+        
+        
+        // for(; i>=0; i--){  //
+        //     if(matrix[i][j] == '1')
+        //     return false;
+        // }
+        return true;
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        placeNQueens(4);
     }
 }
