9590f232a95fea5c80495897a34c5601266ad7d4
Naveen <nk0187912@gmail.com>
2022-03-28 19:32:21 +0530

    Tester file

--- src/Tester.java
+++ src/Tester.java
@@ -1,115 +1,39 @@
 import java.util.*;
 import java.io.*;
 
+
+
 public class Tester {
+  
+  static PrintWriter out = new PrintWriter(System.out);
+  
+  public static int maxArea(int[] arr) {
     
-    static PrintWriter out = new PrintWriter(System.out);
-    public static void placeNQueens(int n){
-        if(n<4)
-            return;
-        
-        int[][] matrix = new int[n][n];
-        
-        
-        //going to test for position of queen in first row, which means every possible desired outcome
-        for(int j = 0; j<n; j++){
-            int i = 0;
-            fill(matrix,i,j,n);
-            printValues(matrix);
-            matrix = new int[n][n];
-        }
-        out.close();
-        
+    int maxArea = 0;
+  
+  
+    int leftMost = 0;
+    int rightMost = arr.length-1;
+  
+  
+    while(leftMost < rightMost){
+      int vertEdge = Math.min(arr[leftMost],arr[rightMost]);
+      int width = leftMost-rightMost;
+    
+      maxArea = Math.max(maxArea, vertEdge*width);
+    
+      if(arr[leftMost]<arr[rightMost])
+        leftMost++;
+      else
+        rightMost--;
     }
-    
-    static void printValues(int[][] matrix){
-        
-        for(int i=0; i<matrix.length; i++){
-            for(int j = 0; j<matrix.length; j++){
-                out.print(matrix[i][j] + " ");
-            }
-        }
-        out.println();
-    }
-    
-    static  void fill(int[][]matrix, int r, int c, int n){
-        
-        HashSet<Integer> set = new HashSet<>();
-        set.add(c);
-        
-        matrix[r][c] = 1;
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(isSafe(matrix,i,j,n,set)){
-                    set.add(j);
-                    matrix[i][j] = 1;
-                }
-            }
-        }
-        
-    }
-    
-    //(m,n) are the coordinate of the current position which is to be checked
-    static boolean isSafe(int[][]matrix, int r, int c, int n, HashSet<Integer> set){
-        
-        int i = r, j = c;
-        
-        //for top and bottom (set has values of j which means index of the coloumn in the matrix which already has a queen)
-        if(set.contains(j))
-            return false;
-        
-        //for top-left direction
-        
-        
-        for(; i>=0; i--){
-            
-            for(;j>=0;j--){
-                if(matrix[i][j] == '1')
-                    return false;
-            }
-        }
-        
-        i=r;j=c;
-        
-        //for top-right direction
-        for(; i>=0; i--){
-            for(;j<n;j++){
-                if(matrix[i][j] == '1')
-                    return false;
-            }
-        }
-        
-        i=r;j=c;
-        //for bottom-left direction
-        for(; i<n; i++){
-            for(;j>=0;j--){
-                if(matrix[i][j] == '1')
-                    return false;
-            }
-        }
-        
-        i=r;j=c;
-        //for bottom-right direction
-        for(; i<n; i++){
-            for(;j<n;j++){
-                if(matrix[i][j] == '1')
-                    return false;
-            }
-        }
-        
-        
-        // for(; i>=0; i--){  //
-        //     if(matrix[i][j] == '1')
-        //     return false;
-        // }
-        return true;
-    }
-    
-    
-    
-    
-    public static void main(String[] args) {
-        placeNQueens(4);
-    }
+  
+    return maxArea;
+  }
+  
+  public static void main(String[] args) {
+      int[]arr= {1,8,6,2,5,4,8,3,7};
+      System.out.println(maxArea(arr));
+   
+  }
 }
