9d18f369f5b1044f5c07904be556421f0cf27738
Naveen <nk0187912@gmail.com>
2022-03-28 19:32:34 +0530

    tester file

--- src/TesterFile.java
+++ src/TesterFile.java
@@ -4,8 +4,16 @@ class ThrowException extends Throwable{}
 
 public class TesterFile{
   public static void main(String[] args) throws ThrowException {
-     String str = new String("123");
-    System.out.println(str.toString());
+  
+    int a = 5, b = 7;
+    
+    while(b!=0){
+      int carry = a&b;
+      a=a^b;
+      b=carry<<1;
+    }
+    //return a;
+    System.out.println(a);
     
   }
 }
